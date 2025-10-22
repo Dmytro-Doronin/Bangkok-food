c94ef477cdb473fcb2a57e6fb8d0d50f776fa08e
Dmytro-Doronin <doronindmytro623@gmail.com>
2025-10-22 19:49:31 +0200

    fix: cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,7 +4,13 @@ export const app = express()
 import cors from 'cors'
 import {productRouter} from "./routes/productRoute";
 
-app.use(cors())
+app.use(
+    cors({
+        origin: "https://bangkok-food-ld5m.vercel.app",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+    })
+)
 app.set('trust proxy', true)
 app.use(express.json())
 
