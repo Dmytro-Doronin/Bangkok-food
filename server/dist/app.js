77b96208727a6692b4fdc77537713c9851f33c17
Dmytro-Doronin <doronindmytro623@gmail.com>
2025-10-22 20:01:15 +0200

    fix: final fix

--- server/dist/app.js
+++ server/dist/app.js
@@ -8,7 +8,11 @@ const express_1 = __importDefault(require("express"));
 exports.app = (0, express_1.default)();
 const cors_1 = __importDefault(require("cors"));
 const productRoute_1 = require("./routes/productRoute");
-exports.app.use((0, cors_1.default)());
+exports.app.use((0, cors_1.default)({
+    origin: "https://bangkok-food-ld5m.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}));
 exports.app.set('trust proxy', true);
 exports.app.use(express_1.default.json());
 exports.app.use('/api/products', productRoute_1.productRouter);
